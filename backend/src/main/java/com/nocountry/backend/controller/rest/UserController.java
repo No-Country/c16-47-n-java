1abad30e8c8de365f510c73d6c6101cd5774f46b
Saulo Cid <118272844+saulocid@users.noreply.github.com>
2024-02-22 11:00:37 -0300

    cambios en endpoints y error en PutMapping

--- backend/src/main/java/com/nocountry/backend/controller/rest/UserController.java
+++ backend/src/main/java/com/nocountry/backend/controller/rest/UserController.java
@@ -1,8 +1,11 @@
 package com.nocountry.backend.controller.rest;
 
 import com.nocountry.backend.model.dto.UserDTO;
+import com.nocountry.backend.model.dto.Request.UsernameDTO;
 import com.nocountry.backend.model.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -12,12 +15,30 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserServiceImpl userService;
+    private final UserServiceImpl US;
 
     // listar usuarios
     @GetMapping(value = "/findall")
     public ResponseEntity<List<UserDTO>> findAll() {
-        return ResponseEntity.ok().body(userService.findAll());
+        return ResponseEntity.ok().body(US.findAll());
+    }
+
+    // buscar por username
+    @GetMapping(value = "/findusername")
+    public ResponseEntity<Object> findByUsername(@RequestParam String username) {
+        return ResponseEntity.ok().body(US.findByUsername(username));
+    }
+
+    // buscar por email
+    @GetMapping(value = "/findemail")
+    public ResponseEntity<Object> findByEmail(@RequestParam String email) {
+        return ResponseEntity.ok().body(US.findByEmail(email));
+    }
+
+    @PutMapping(value = "/updateusename")
+    public ResponseEntity<Object> updateUsername(@RequestBody UsernameDTO request) {
+        // US.updateUsername(request); // hay que arreglar lo que dije de las exceptions
+        return ResponseEntity.status(HttpStatus.UPGRADE_REQUIRED).body(null);
     }
 
 }
