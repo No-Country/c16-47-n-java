378a340fc5b5e05fa6c01afdb206d54056f585d3
Saulo Cid <118272844+saulocid@users.noreply.github.com>
2024-02-25 14:18:37 -0300

    modify: arreglo de request, modificacion de metodos en UserService y UserServuceImpl, creacion de metodos en controlador UserController

--- backend/src/main/java/com/nocountry/backend/controller/rest/AuthController.java
+++ backend/src/main/java/com/nocountry/backend/controller/rest/AuthController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.nocountry.backend.model.dto.Request.AuthResponse;
-import com.nocountry.backend.model.dto.Request.LoginDTO;
-import com.nocountry.backend.model.dto.Request.RegisterDTO;
+
+import com.nocountry.backend.model.dto.Request.LoginRequest;
+import com.nocountry.backend.model.dto.Request.RegisterRequest;
+import com.nocountry.backend.model.dto.Response.AuthResponse;
 import com.nocountry.backend.model.service.AuthService;
 import lombok.RequiredArgsConstructor;
 
@@ -20,13 +21,13 @@ public class AuthController {
     
     // inicia sesion al usuario desde el AuthService
     @PostMapping(value = "login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginDTO request) {
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(AS.login(request));
     }
 
     // guarda-crea el usuario desde el AuthService
     @PostMapping(value = "register")
-    public ResponseEntity<AuthResponse> register(@RequestBody RegisterDTO request) {
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(AS.register(request));
     }
 
--- backend/src/main/java/com/nocountry/backend/controller/rest/UserController.java
+++ backend/src/main/java/com/nocountry/backend/controller/rest/UserController.java
@@ -1,11 +1,14 @@
 package com.nocountry.backend.controller.rest;
 
 import com.nocountry.backend.model.dto.UserDTO;
-import com.nocountry.backend.model.dto.Request.UsernameDTO;
+import com.nocountry.backend.model.dto.Request.AddressRequest;
+import com.nocountry.backend.model.dto.Request.CellphoneRequest;
+import com.nocountry.backend.model.dto.Request.EmailRequest;
+import com.nocountry.backend.model.dto.Request.PasswordRequest;
+import com.nocountry.backend.model.dto.Request.UsernameRequest;
+import com.nocountry.backend.model.entity.UserEntity;
 import com.nocountry.backend.model.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -17,28 +20,70 @@ public class UserController {
 
     private final UserServiceImpl US;
 
-    // listar usuarios
+    // listar usuarios ADMIN
     @GetMapping(value = "/findall")
     public ResponseEntity<List<UserDTO>> findAll() {
         return ResponseEntity.ok().body(US.findAll());
     }
 
-    // buscar por username
+    // buscar por username ADMIN
     @GetMapping(value = "/findusername")
     public ResponseEntity<UserDTO> findByUsername(@RequestParam String username) {
         return ResponseEntity.ok().body(US.findByUsername(username));
     }
 
-    // buscar por email
+    // buscar por email ADMIN
     @GetMapping(value = "/findemail")
     public ResponseEntity<UserDTO> findByEmail(@RequestParam String email) {
         return ResponseEntity.ok().body(US.findByEmail(email));
     }
 
+    // traer usuario USER
+    @GetMapping(value = "/user")
+    public ResponseEntity<UserDTO> findUser(@RequestBody UserEntity user) {
+        return ResponseEntity.ok().body(US.getOne(user));
+    }
+
+    // modificar username USER
     @PutMapping(value = "/updateusename")
-    public ResponseEntity<Object> updateUsername(@RequestBody UsernameDTO request) {
-        // US.updateUsername(request); // hay que arreglar lo que dije de las exceptions
-        return ResponseEntity.status(HttpStatus.UPGRADE_REQUIRED).body(null);
+    public ResponseEntity<?> updateUsername(@RequestBody UsernameRequest request) {
+        US.updateUsername(request);
+        return ResponseEntity.ok().body(null);
+    }
+
+    // modificar email USER
+    @PutMapping(value = "/updateemail")
+    public ResponseEntity<?> updateEmail(@RequestBody EmailRequest request) {
+        US.updateEmail(request);
+        return ResponseEntity.ok().body(null);
+    }
+
+    // modificar password USER
+    @PutMapping(value = "/updatepassword")
+    public ResponseEntity<?> updatePasswrod(@RequestBody PasswordRequest request) {
+        US.updatePassword(request);
+        return ResponseEntity.ok().body(null);
+    }
+
+    // modificar cellphone USER
+    @PutMapping(value = "/updatecellphone")
+    public ResponseEntity<?> updateCellphone(@RequestBody CellphoneRequest request) {
+        US.updateCellphone(request);
+        return ResponseEntity.ok().body(null);
+    }
+
+    // modificar address USER
+    @PutMapping(value = "/updateaddress")
+    public ResponseEntity<?> updateAddress(@RequestBody AddressRequest request) {
+        US.updateAddress(request);
+        return ResponseEntity.ok().body(null);
+    }
+
+    // borrar usuario ADMIN
+    @DeleteMapping(value = "/delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        US.deleteUser(id);
+        return ResponseEntity.ok().body(null);
     }
 
 }
