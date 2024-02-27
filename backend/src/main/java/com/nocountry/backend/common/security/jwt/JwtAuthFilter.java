f25a2d7c2246fbc977a26653881e644c5cba4c42
xOnlinEx <daniel.floresvargas666@gmail.com>
2024-02-27 00:21:46 -0400

    add: profiles docker and dev

--- backend/src/main/java/com/nocountry/backend/common/security/jwt/JwtAuthFilter.java
+++ backend/src/main/java/com/nocountry/backend/common/security/jwt/JwtAuthFilter.java
@@ -2,6 +2,7 @@ package com.nocountry.backend.common.security.jwt;
 
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
