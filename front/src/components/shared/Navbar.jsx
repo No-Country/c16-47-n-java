350c5cf0a71492a84827394fe4a3e39e9304aa4e
Micaela Murrie <micaelamurrie@gmail.com>
2024-02-27 22:53:32 -0300

    mas margen en nav

--- front/src/components/shared/Navbar.jsx
+++ front/src/components/shared/Navbar.jsx
@@ -4,13 +4,13 @@ function Navbar() {
   return (
     <nav className="flex justify-end pr-8 shadow-md">
       <ul className="flex mb-4">
-        <Link className="mr-4" to="/">
+        <Link className="mr-5" to="/">
           Home
         </Link>
-        <Link className="mr-4" to="/contacto">
+        <Link className="mr-5" to="/contacto">
           Contacto
         </Link>
-        <Link className="mr-4" to="/user">
+        <Link className="mr-5" to="/user">
           Editar Perfil
         </Link>
         <Link to="/login">Login</Link>
