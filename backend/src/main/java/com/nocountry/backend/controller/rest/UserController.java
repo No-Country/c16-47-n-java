33fb230c1dbdb33607a3c3c5186538194b322733
Saulo Cid <118272844+saulocid@users.noreply.github.com>
2024-03-03 18:34:44 -0300

    manejo de lógica de negocio en imagen en el Profile.jsx

--- backend/src/main/java/com/nocountry/backend/controller/rest/UserController.java
+++ backend/src/main/java/com/nocountry/backend/controller/rest/UserController.java
@@ -1,6 +1,5 @@
 package com.nocountry.backend.controller.rest;
 
-import com.nocountry.backend.model.dto.Request.ImageRequest;
 import com.nocountry.backend.model.dto.Request.PasswordRequest;
 import com.nocountry.backend.model.dto.Request.ProfileRequest;
 import com.nocountry.backend.model.dto.Response.UserResponse;
@@ -14,6 +13,7 @@ import java.io.IOException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -39,12 +39,12 @@ public class UserController {
     }
 
     // setear imagen
-    @PostMapping("/updateImage")
-    public ResponseEntity<Void> upload(@RequestBody ImageRequest request) throws IOException {
-        var image = imageService.save(cloudinaryService.upload(request.getFile()));
-        UserEntity usuario = UR.findById(request.getId()).orElseThrow();
+    @PostMapping("/updateImage/{id}")
+    public synchronized ResponseEntity<String> upload(@RequestParam("image") MultipartFile imagen, @PathVariable Long id) throws IOException {
+        var image = imageService.save(cloudinaryService.upload(imagen));
+        UserEntity usuario = UR.findById(id).orElseThrow();
         usuario.setImageUrl(image.getUrl());
-        return ResponseEntity.ok().body(null);
+        return ResponseEntity.ok().body(usuario.getImageUrl());
     }
 
     @PutMapping(value = "/profileUpdate")
