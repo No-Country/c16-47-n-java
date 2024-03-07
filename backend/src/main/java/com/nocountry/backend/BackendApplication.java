8f77af17a79e8d6974409ab75d74d7df08ba1657
Joel Benjamín Fiaré <joelfiare@gmail.com>
2024-03-07 16:51:00 -0300

    mensaje al final indicando que la aplicacion se ha iniciado correctamente

--- backend/src/main/java/com/nocountry/backend/BackendApplication.java
+++ backend/src/main/java/com/nocountry/backend/BackendApplication.java
@@ -2,12 +2,31 @@ package com.nocountry.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+
+//@SpringBootApplication
+//public class BackendApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(BackendApplication.class, args);
+//	}
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication app = new SpringApplication(BackendApplication.class);
+		app.addListeners(new CustomApplicationListener());
+		app.run(args);
 	}
 
+	static class CustomApplicationListener implements ApplicationListener<ContextRefreshedEvent> {
+		@Override
+		public void onApplicationEvent(ContextRefreshedEvent event) {
+			System.out.println("---------------------------------------------");
+			System.out.println("¡La aplicación se ha iniciado correctamente!");
+			System.out.println("---------------------------------------------");
+		}
+	}
 }
